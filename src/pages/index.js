f0444fb51e260607da352f40c57aac1a31bf32b6
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-09-08 19:19:29 +0100

    Add use case section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import landing_img from "../svg/landing-illutration.svg"
+import gmail from "../svg/gmail.svg"
+import messenger from "../svg/messenger.svg"
+import yahoo from "../svg/yahoo.svg"
+import outlook from "../svg/outlook.svg"
+import twitter from "../svg/twitter.svg"
+import medium from "../svg/medium.svg"
+import linkedin from "../svg/linkedin.svg"
+import facebook from "../svg/facebook.svg"
+import google_docs from "../svg/google-docs.svg"
+import slack from "../svg/slack.svg"
 import styles from "./index.module.scss"
 
 function LandingSection() {
@@ -29,24 +39,56 @@ function LandingSection() {
   )
 }
 
+function SampleTextarea({ title, input }) {
+  return (
+    <div>
+      <h4 className={styles.sampleSection__title}>{title}</h4>
+      <textarea
+        className={styles.sampleSection__textarea}
+        value={input}
+      ></textarea>
+    </div>
+  )
+}
+
 function SampleSection() {
   return (
     <div className={styles.sampleSection}>
+      <SampleTextarea
+        title="Check Gender Sensitivity"
+        input="Mankind is blessed with so much resources"
+      />
+      <SampleTextarea
+        title="GenderWise"
+        input="Humanity is blessed with so much resources"
+      />
+    </div>
+  )
+}
+
+function UsecaseSection({ text, image }) {
+  function Icon({ icon }) {
+    return (
       <div>
-        <h4 className={styles.sampleSection__title}>
-          Check Gender Sensitivity
-        </h4>
-        <textarea
-          className={styles.sampleSection__textarea}
-          value="Mankind is blessed with so much resources"
-        ></textarea>
+        <img src={icon} alt="" />
       </div>
-      <div>
-        <h4 className={styles.sampleSection__title}>GenderWise</h4>
-        <textarea className={styles.sampleSection__textarea} value="Humanity is blessed with so much resources"></textarea>
+    )
+  }
+
+  return (
+    <div className={styles.usecaseSection}>
+      <h3 className={styles.usecaseSection__text}>{text}</h3>
+      <div className={styles.usecaseSection__icon}>
+        {image.map((icon, index) => (
+          <Icon icon={icon} key={index} />
+        ))}
       </div>
     </div>
   )
+}
+
+function Footer() {
+  return <div className={styles.footer}>2020 &copy; GenderWise</div>
 }
 
 export default function Home() {
@@ -54,6 +96,20 @@ export default function Home() {
     <Layout>
       <LandingSection />
       <SampleSection />
+      <UsecaseSection
+        text="Emails and Messages"
+        image={[gmail, messenger, yahoo, outlook]}
+      />
+      <UsecaseSection
+        text="Social Media, Articles and Blogposts"
+        image={[twitter, medium, linkedin, facebook]}
+      />
+      <UsecaseSection
+        text="Projects and Documents"
+        image={[google_docs, slack]}
+      />
+
+      <Footer />
     </Layout>
   )
 }
